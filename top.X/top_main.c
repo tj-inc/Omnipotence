5c86f2f743f6ef4122e9cc2dbbdcf0ff8ea1adea
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2016-05-18 20:37:17 -0400

    [Joe] Trigger code now works.

--- top.X/top_main.c
+++ top.X/top_main.c
@@ -48,8 +48,11 @@
 
 // Trigger
 #define pull_trigger RC1
-#define PWM_PERIOD 20000
-enum Pulse_Widths {MIN_HIGH = 500, FORTYFIVE_HIGH = 1003, NINTY_HIGH = 1505, ONETHIRTYFIVE_HIGH = 2008, MAX_HIGH = 2510};
+#define PWM_PERIOD 5000
+#define TWOFIFTY_MS 62500
+#define TRIG_DELAY_MULTIPLIER 5
+enum Pulse_Widths {MIN_HIGH = 125, FORTYFIVE_HIGH = 251, NINTY_HIGH = 376, ONETHIRTYFIVE_HIGH = 502, MAX_HIGH = 627};
+enum Trigger_States {Trigger_StandBy, Trigger_Pulled, Trigger_CoolDown};
 
 // RGB Module
 #define RGB_R RC4
@@ -73,13 +76,21 @@ unsigned int last_WD_time;
 char system_state = SYSTEM_INIT;
 
 // Trigger
-unsigned int high_pulse = 0;
+unsigned int high_pulse = MIN_HIGH;
+char trigger_state = Trigger_StandBy;
+char counter = 0;
+
+//// Async Task Management Module
+//struct Task delays[11];
+//char task_index = 1;
 
 void main(void) {
     // Initialize RC0 and RC1 for mode and pull_trigger, and RC6:4 for RGB
     ANSEL = 0;
     ANSELH = 0;
-    TRISC = 0b1110111;
+//    TRISC = 0b1110011;
+    TRISC = 0xFF;
+    TRISC2 = 0;
     
     // TDP Module
     TRISA = 0b111;
@@ -87,7 +98,7 @@ void main(void) {
     // WD Module
     TRISB = 0b111;
 //    nRBPU = 0;
-    IOCB = 0b111;
+//    IOCB = 0b111;
     WD_last_left = WD_LEFT;
     WD_last_center = WD_CENTER;
     WD_last_right = WD_RIGHT;
@@ -97,33 +108,61 @@ void main(void) {
     // Init Timer 1
     TMR1GE = 0; TMR1ON = 1; 			//Enable TIMER1 (See Fig. 6-1 TIMER1 Block Diagram in PIC16F887 Data Sheet)
 	TMR1CS = 0; 					//Select internal clock whose frequency is Fosc/4, where Fosc = 8 MHz
-	T1CKPS1 = 0; T1CKPS0 = 1; 		 	//Set prescale to divide by 2 yielding a clock tick period of 1 microseconds
+	T1CKPS1 = 1; T1CKPS0 = 1; 		 	//Set prescale to divide by 8 yielding a clock tick period of 4 microseconds
     
     // Init CCP1 for PWM
-    CCP1M3 = 0;
-    CCP1M2 = 0;
-    CCP1M1 = 1;
-    CCP1M0 = 0;
+    CCP1M3 = 0; CCP1M2 = 0; CCP1M1 = 1; CCP1M0 = 0;
     CCP1IE = 1;
 	CCP1IF = 0;
     
+    // Init CCP2 for trigger timeout
+    CCP2M3 = 1; CCP2M2 = 0; CCP2M1 = 1; CCP2M0 = 0;
+    CCP2IE = 0; // We don't enable the interrupt yet
+	CCP2IF = 0;
+    
     // Delay 1 minute to prepare the sensors
-    for (long i = 1; i < TDP_ONE_MIN; i++);
+//    for (long i = 1; i < TDP_ONE_MIN; i++);
     
     // Turn on Interrupts
+    CCPR1 = CCPR1 + 100;
     PEIE = 1;
 	GIE = 1;
     while (1) {
         if (mode) {
             // This is auto mode
+            // Trigger FSM
             
+        }
+        
+        switch (trigger_state) {
+            case Trigger_StandBy:
+                if (pull_trigger == 0) {
+                    trigger_state = Trigger_Pulled;
+                    counter = 0;
+                }
+                high_pulse = MIN_HIGH;
+                break;
+            case Trigger_Pulled:
+                if (CCP2IE == 0) {
+                    CCPR2 = CCPR2 + TWOFIFTY_MS;
+                    CCP2IE = 1;
+                }
+                high_pulse = MAX_HIGH;
+                break;
+            case Trigger_CoolDown:
+                if (CCP2IE == 0) {
+                    CCPR2 = CCPR2 + TWOFIFTY_MS;
+                    CCP2IE = 1;
+                }
+                high_pulse = MIN_HIGH;
+                break;
         }
     }
 }
 
 void interrupt interrupt_handler() {
-    if (CCP1IF == 1) {
-        if (RC2 == 1) {
+    if (CCP1IF) {
+        if (RC2) {
             CCPR1 = CCPR1 + high_pulse;
         } else {
             CCPR1 = CCPR1 + PWM_PERIOD - high_pulse;
@@ -131,11 +170,34 @@ void interrupt interrupt_handler() {
         CCP1IF = 0;
     }
     
+    if (CCP2IF) {
+        switch (trigger_state) {  // Do I know the current state is always right when I'm here
+            case Trigger_Pulled:
+                if (counter == TRIG_DELAY_MULTIPLIER) {
+                    trigger_state = Trigger_CoolDown;
+                    counter = 0;
+                    CCP2IE = 0;
+                } else {
+                    counter++;
+                }
+                break;
+            case Trigger_CoolDown:
+                if (counter == TRIG_DELAY_MULTIPLIER) {
+                    trigger_state = Trigger_StandBy;
+                    CCP2IE = 0;
+                } else {
+                    counter++;
+                }
+                break;
+        }
+        CCP2IF = 0;
+    }
+    
     if (RBIF) {
         last_WD_time = TMR1;
         WD_last_left = WD_LEFT;
         WD_last_center = WD_CENTER;
         WD_last_right = WD_RIGHT;
-        RBIF = 1;
+        RBIF = 0;
     }
 }
